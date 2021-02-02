50e2579af49530f51c06d735711de8b021290d1d
Azimka228 <71658005+Azimka228@users.noreply.github.com>
2021-02-02 20:24:13 +0300

    js fix

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ window.addEventListener('scroll', () => {
 
 
 
-    document.querySelectorAll("header,section").forEach((el, i) => {
+    document.querySelectorAll("section").forEach((el, i) => {
         if (el.offsetTop - document.querySelector(".header_nav").clientHeight <= ScrollDistance) {
             document.querySelectorAll(".header_nav a").forEach((el) => {
                 if (el.classList.contains("active")) {
@@ -18,28 +18,39 @@ window.addEventListener('scroll', () => {
 
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+    const prev = document.getElementById('btn_prev'),
+        next = document.getElementById('btn_next'),
+        slides = document.querySelectorAll('.main_content');
 
-const prev = document.getElementById('btn_prev'),
-    next = document.getElementById('btn_next'),
-    slides = document.querySelectorAll('.main_content');
+    let index = 0;
+    console.log(prev);
+    const activeSlide = n => {
+        for (slide of slides) {
+            slide.classList.remove('selected');
 
-let index = 0;
-console.log(prev);
-const activeSlide = n => {
-    for (slide of slides) {
-        slide.classList.remove('selected');
-
+        }
+        slides[n].classList.add('selected');
     }
-    slides[n].classList.add('selected');
-}
 
-const nextSlide = () => {
-    if (index == slides.length - 1) {
-        index = 0;
-        Activeslide(index);
-    } else {
-        index++;
-        Activeslide(index);
+    const nextSlide = () => {
+        if (index == slides.length - 1) {
+            index = 0;
+            activeSlide(index);
+        } else {
+            index++;
+            activeSlide(index);
+        }
     }
-}
-next.addEventListener('click', nextSlide);
+    const prevslide = () => {
+        if (index == 0) {
+            index = slides.length - 1;
+            activeSlide(index);
+        } else {
+            index--;
+            activeSlide(index);
+        }
+    }
+    next.addEventListener('click', nextSlide)
+    prev.addEventListener('click', prevslide)
+});
