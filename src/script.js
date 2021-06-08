968a8cf868b1c3edea618c10c519f5af43c1c3d1
Azimka228 <71658005+Azimka228@users.noreply.github.com>
2021-06-08 21:06:08 +0300

    Fix

--- src/script.js
+++ src/script.js
@@ -1,21 +1,18 @@
-import './css/normalize.css'
-import './css/style.css'
-import image from './image/arrow-left.svg'
+import styles from './css/style.css';
 
-console.log(image)
+
 window.addEventListener('scroll', () => {
     let ScrollDistance = window.scrollY;
 
 
 
     document.querySelectorAll("section").forEach((el, i) => {
-        if (el.offsetTop - document.querySelector(".header_nav").clientHeight <= ScrollDistance) {
+        if (el.offsetTop - document.querySelector(".header_fixed").clientHeight <= ScrollDistance) {
             document.querySelectorAll(".header_nav a").forEach((el) => {
                 if (el.classList.contains("active")) {
                     el.classList.remove("active");
                 }
             });
-
             document.querySelectorAll(".header_nav li")[i].querySelector("a").classList.add("active")
         }
     });
@@ -23,17 +20,17 @@ window.addEventListener('scroll', () => {
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-    const prev = document.getElementById('btn_prev'),
-        next = document.getElementById('btn_next'),
-        slides = document.querySelectorAll('.main_content'),
+    const prev = document.querySelector('.slider__button_left'),
+        next = document.querySelector('.slider__button_right'),
+        slides = document.querySelectorAll('.slides'),
         Main = document.querySelector('.main'),
         PTags = document.querySelectorAll(".tag"),
         Hmenu = document.querySelector('.hamburger-menu'),
-        HnavMenu = document.querySelector('.header_nav_menu');
+        HnavMenu = document.querySelector('.header_nav');
 
     let index = 0;
     const activeSlide = n => {
-        for (slide of slides) {
+        for (let slide of slides) {
             slide.classList.remove('selected');
 
         }
@@ -50,7 +47,10 @@ document.addEventListener("DOMContentLoaded", () => {
             activeSlide(index);
         }
     }
-    const prevslide = () => {
+
+    next.addEventListener('click', nextSlide);
+
+    const prevSlide = () => {
         if (index == 0) {
             index = slides.length - 1;
             activeSlide(index);
@@ -59,60 +59,68 @@ document.addEventListener("DOMContentLoaded", () => {
             activeSlide(index);
         }
     }
-    next.addEventListener('click', nextSlide)
-    prev.addEventListener('click', prevslide)
-    /* slider bg*/
-    document.querySelector('.main__arrow-right').onclick = function () {
-        if (Main.classList.contains("main_bg")) {
-            Main.classList.remove("main_bg")
-        } else {
-            Main.classList.add("main_bg")
-        }
-    }
-    document.querySelector('.main__arrow-left').onclick = function () {
-        if (Main.classList.contains("main_bg")) {
-            Main.classList.remove("main_bg")
-        } else {
-            Main.classList.add("main_bg")
-        }
+    prev.addEventListener('click', prevSlide);
 
-    }
+
     Hmenu.onclick = function (e) {
         e.preventDefault();
-
-        Hmenu.classList.toggle("hamburger-menu_img_opened")
-        if (HnavMenu.classList.contains("header_nav_menu_opened")) {
-            HnavMenu.classList.remove("header_nav_menu_opened")
+        if (HnavMenu.classList.contains("header_nav_opened")) {
+            HeaderNavClose()
         } else {
-            HnavMenu.classList.add("header_nav_menu_opened")
+            HnavMenu.classList.add("header_nav_opened");
+            HeaderModal.classList.add("header_nav_modal_opened");
+            Hmenu.classList.add("hamburger-menu_img_opened");
         }
-        if (Hmenu !== e.target) {
-            Hmenu.classList.remove("header_nav_menu_opened");
-        }
-
     }
-    /* chips */
 
 
-    function SetupChips (){
-        const ProfileBTN = document.querySelectorAll('.tag');
-        const ProfilePhoto = document.querySelectorAll ('.avatar');
-        ProfileBTN.forEach(button => {
-            button.addEventListener('click', (e) => {
-                for (let i = 0; i < ProfileBTN.length; i++) {
-                    ProfileBTN[i].classList.remove('tag_active')
-                }
-                e.target.classList.add('tag_active');
-                ProfilePhoto.forEach (image => {
-                   if (image.dataset.tag === e.target.dataset.tagValue || e.target.dataset.tagValue === 'All') {
+
+    function HeaderNavClose() {
+        HnavMenu.classList.remove("header_nav_opened");
+        HeaderModal.classList.remove("header_nav_modal_opened");
+        Hmenu.classList.remove("hamburger-menu_img_opened");
+    }
+    let HeaderModal = document.querySelector('.header_nav_modal');
+    HeaderModal.addEventListener("click", e => {
+        HeaderNavClose()
+    });
+
+    document.querySelectorAll('.header_nav li a').forEach(item => {
+        item.addEventListener('click', event => {
+            HeaderNavClose()
+        })
+      })
+
+
+
+/* chips */
+
+
+function SetupChips() {
+    const ProfileBTN = document.querySelectorAll('.tag');
+    const ProfilePhoto = document.querySelectorAll('.avatar');
+    ProfileBTN.forEach(button => {
+        button.addEventListener('click', (e) => {
+            for (let i = 0; i < ProfileBTN.length; i++) {
+                ProfileBTN[i].classList.remove('tag_active')
+            }
+            e.target.classList.add('tag_active');
+            ProfilePhoto.forEach(image => {
+                if (image.dataset.tag === e.target.dataset.tagValue || e.target.dataset.tagValue === 'All') {
                     image.style = '';
-                   } else {
+                } else {
                     image.style = 'display: none';
-                   }
-                
-                })
-            });
+                }
+
+            })
         });
-     }
-     SetupChips ();
+    });
+}
+SetupChips();
+let FormQuote = document.querySelector('.form_contact')
+FormQuote.addEventListener('click', (e) => {
+    if (e.target.dataset.form === 'send') {
+        console.log('dsd')
+    }
+})
 });
